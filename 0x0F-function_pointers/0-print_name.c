131628185fbac85e84e60fbf190b42b426fc50c2
Mostafa09876 <mon2599380@gmail.com>
2023-04-19 17:20:05 -0700

    g

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+/**
+ * print_name - function prints name
+ *
+ * @name: string to print
+ * @f: function pointer
+ *
+ * Return: nothing
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
